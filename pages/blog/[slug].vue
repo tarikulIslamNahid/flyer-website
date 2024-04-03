13db4b4cc609410dca8c1a95ee764f792c4b85b5
tarikulIslamNahid <jttarekulnahid@gmail.com>
2024-04-03 13:03:54 +0600

    blog and blog details  page done

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,223 @@
+<script setup>
+definePageMeta({
+    layout: 'website',
+});
+useHead({
+  title: `Blog `,
+});
+ 
+
+
+</script>
+
+<template>
+    <main>
+        <!-- breadcrumb area start -->
+        <div class="brandcrumb-area-2 breadcrumb-area-space-2 pt-290 pb-205 mb-120" data-overlay="theme-2"
+            data-opacity="7">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="breadcrumb-content text-center">
+                            <h3 class="service-breadcrumb-title" data-color="#fff">Insight and advice from our expert team.</h3>
+                            <div class="service-breadcrumb-meta">
+                                <span class="service-breadcrumb-meta-single" data-color="#fff"><i class="fal fa-clock"></i>09
+                                    March 2022</span>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- breadcrumb area end -->
+        <!-- blog area start -->
+        <div class="blog-area pb-120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12">
+                        <div class="blog-content-main">
+                            <p>Fanny around chancer that pardon you bamboozled morish jolly good, is a blinding shot
+                                naff ruddy plastered bloke quaint, Eaton I don't want no agro bite your arm off amongst
+                                bugger all mate. Matie boy young delinquent spiffing chancer cuppa in my flat Harry naff
+                                bonnet blatant Why mush spiffing good time, buggered A bit your father bleeding gosh
+                                have it chip shop me old mucker fanny around cracking goal blag. Morish what a load of
+                                rubbish Harry blatant the BBC horse play tinkety tonk old fruit pardon me zonked chip
+                                shop, smashing the wireless skive off goal barney.</p>
+                            <div class="thumb mb-40">
+                                <img src="/assets/img/blog/b-details-2.webp" alt="image not found">
+                            </div>
+                            <h4 class="blog-content-title">An exciting update on our acquisition.</h4>
+                            <p class="mb-40">Hunky-dory bits and bobs pardon you cup of tea chip shop tickety-boo happy
+                                days a load of
+                                old tosh ruddy gutted mate, brown bread old on your bike mate bum bag barney amongst
+                                mufty cheesed off fanny around, lemon squeezy lurgy blow off James Bond in my flat
+                                blatant Queen's English bugger. Why me old mucker so I said some dodgy chav a buggered
+                                faff about fanny around bodge, that burke golly gosh chimney pot cor blimey guvnor
+                                gutted standard he legged ruddy cheeky.</p>
+                            <div class="single-blog-list mb-40">
+                                <div class="blockquote-shadow blockquote-wrap">
+                                    <div class="quote-shapes">
+                                        <img src="/assets/img/icon/quote-1.webp" alt="image not found">
+                                        <img src="/assets/img/icon/quote-2.webp" alt="image not found">
+                                    </div>
+                                    <blockquote>‘‘Haggle squiffy some dodgy chav nice one chimney pot crikey
+                                        hotpot he
+                                        nicked young.’’</blockquote>
+                                    <h5 class="blockquote-author-name">Max Conversion</h5>
+                                </div>
+                            </div>
+                            <p>Tomfoolery gosh I don't want no agro Richard amongst chimney pot cor blimey guvnor is you
+                                mug daft, some dodgy chav I nancy boy knackered old happy days owt to do with me, Why
+                                cuppa easy peasy gutted mate chap bite your arm off bobby. Crikey tinkety tonk old fruit
+                                a off his bender amongst agabond spend.</p>
+                            <div class="blog-meta flex-blog-meta has-border-top-1 mb-55">
+                                <div class="meta-tags">
+                                    <h3 class="meta-sm-title d-inline-block">Tags:</h3>
+                                    <div class="tagcloud d-inline-block">
+                                        <a href="blog-list.html">Optimiation</a>
+                                        <a href="blog-list.html">The Saas</a>
+                                        <a href="blog-list.html">Design</a>
+                                    </div>
+                                </div>
+                                <div class="meta-social text-end">
+                                    <h3 class="meta-sm-title d-inline-block">Share:</h3>
+                                    <div class="social-follow social-follow-2 social-follow-space d-inline-block">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-vimeo-v"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-post-navigation mb-50">
+                                <a href="blog-details.html">
+                                    <div class="post-navigation-box">
+                                        <span class="post-navigation-redirect"><i
+                                                class="fal fa-arrow-left"></i>Preovious
+                                            Post</span>
+                                        <h5 class="post-navigation-label">Social media strategies</h5>
+                                    </div>
+                                </a>
+                                <a href="blog-details.html">
+                                    <div class="post-navigation-box text-start  text-sm-end" data-bg-color="#eeeff4">
+                                        <span class="post-navigation-redirect text-start  text-md-end"
+                                            data-color="#535559">Next
+                                            Post<i class="fal fa-arrow-right  right-redirect"></i></span>
+                                        <h5 class="post-navigation-label text-start text-md-end" data-color="#0a1426">
+                                            Social media
+                                            strategies
+                                        </h5>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="author-biography mb-75">
+                                <div class="author-biography-img"><img src="/assets/img/icon/author-biography.webp"
+                                        alt="image not found"></div>
+                                <div class="author-biography-content">
+                                    <h5 class="author-biography-title">Fleece Marigold</h5>
+                                    <p class="author-biography-info">Do one bamboozled on your bike mate in my flat
+                                        boot nicked is gutted mate morish no biggie golly gosh bog jolly.</p>
+                                </div>
+                            </div>
+                            <div class="related-news-area pb-80">
+                                <div class="row">
+                                    <div class="col-xxl-12">
+                                        <h5 class="blog-sm-heading mb-35">Related News</h5>
+                                    </div>
+                                </div>
+                                <div class="row g-20">
+                                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 mb-30 mb-md-0">
+                                        <div class="related-news-box wow fadeInUp" data-wow-delay=".2s">
+                                            <div class="thumb">
+                                                <img src="/assets/img/blog/blog-details-1.webp" alt="image not found">
+                                            </div>
+                                            <div class="content">
+                                                <div class="news-meta meta-related-space">
+                                                    <div class="single-meta meta-sm">
+                                                        <span class="news-date">20 March 2022</span>
+                                                    </div>
+                                                    <div class="single-meta  meta-sm">
+                                                        <a href="blog-details.html" class="news-comment">3 Comment</a>
+                                                    </div>
+                                                </div>
+                                                <h4 class="related-news-title"><a href="blog-details.html">Integrate
+                                                        your favorite Apps
+                                                        with SaaSup</a></h4>
+                                                <a href="blog-details.html" class="related-read-more">Read More<i
+                                                        class="fal fa-arrow-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 mb-30 mb-md-0">
+                                        <div class="related-news-box wow fadeInUp" data-wow-delay=".4s">
+                                            <div class="thumb">
+                                                <img src="/assets/img/blog/blog-details-2.webp" alt="image not found">
+                                            </div>
+                                            <div class="content">
+                                                <div class="news-meta meta-related-space">
+                                                    <div class="single-meta meta-sm">
+                                                        <span class="news-date">20 March 2022</span>
+                                                    </div>
+                                                    <div class="single-meta  meta-sm">
+                                                        <a href="blog-details.html" class="news-comment">3 Comment</a>
+                                                    </div>
+                                                </div>
+                                                <h4 class="related-news-title"><a href="blog-details.html">Inhouse
+                                                        Design Thinking
+                                                        How I deal with it?</a></h4>
+                                                <a href="blog-details.html" class="related-read-more">Read More<i
+                                                        class="fal fa-arrow-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 mb-30 mb-md-0">
+                                        <div class="related-news-box wow fadeInUp" data-wow-delay=".6s">
+                                            <div class="thumb">
+                                                <img src="/assets/img/blog/blog-details-3.webp" alt="image not found">
+                                            </div>
+                                            <div class="content">
+                                                <div class="news-meta meta-related-space">
+                                                    <div class="single-meta meta-sm">
+                                                        <span class="news-date">20 March 2022</span>
+                                                    </div>
+                                                    <div class="single-meta  meta-sm">
+                                                        <a href="blog-details.html" class="news-comment">3 Comment</a>
+                                                    </div>
+                                                </div>
+                                                <h4 class="related-news-title"><a href="blog-details.html">An exciting
+                                                        update on our
+                                                        acquisition.</a></h4>
+                                                <a href="blog-list.html" class="related-read-more">Read More<i
+                                                        class="fal fa-arrow-right"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /. related news -->
+                                     <!-- cta area start -->
+        <div class="cta-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xxl-12">
+                        <div class="sasup-cta-content-5">
+                            <h3 class="sasup-cta-title-5">Ready to dive in?<br> Start your <span>Free</span>
+                                trial today.</h3>
+                            <div class="sasup-cta-action-wrapper-5">
+                                <a class="sasup-cta-action-link-5" href="sign-up.html">Get Started</a>
+                                <a class="sasup-cta-action-transparent-link-5" href="about.html">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- cta area end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- blog area end -->
+    </main>
+</template> 
---
+++ pages/blog/index.vue
@@ -0,0 +1,163 @@
+<script setup>
+definePageMeta({
+    layout: 'website',
+});
+useHead({
+  title: `Blog `,
+});
+ 
+
+
+</script>
+
+<template>
+    <main>
+        <!-- breadcrumb area start -->
+        <div class="breadcrumb-area pt-215 pb-140 mb-110 pb-130" data-background="/assets/img/bg/breadcrumb_bg.webp">
+            <div class="container">
+                <div class="breadcrumb-content text-center">
+                    <h3 class="breadcrumb-title">Blogs</h3>
+                    <div class="breadcrumb-trail">
+                        <nav>
+                            <ul class="trail-items">
+                                <li class="trail-item">
+                                    <nuxt-link to="/"><span>Home</span></nuxt-link>
+                                    </li>
+                                <li class="trail-item"><span>Blogs</span></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- breadcrumb area end -->
+        <!-- blog area start -->
+        <div class="blog-area pb-120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                        <div class="sasup-blog-box-4 mb-30 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="sasup-blog-box-img-4">
+                                <a href="blog-details.html"><img src="/assets/img/blog/blog-4-1.webp" alt="image not found"></a>
+                            </div>
+                            <div class="sasup-blog-box-content-4 pt-25">
+                                <a href="blog-details.html" class="sasup-blog-box-category-4 mb-10">Design Tools</a>
+                                <h4 class="sasup-blog-box-title-4">
+                                    <nuxt-link to="/blog/1">A guide to market your Saas product</nuxt-link>
+                                 </h4>
+                            </div>
+                            <div class="sasup-blog-box-meta-4">
+                                <span>October 28, 2022</span>
+                                <span>4 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                        <div class="sasup-blog-box-4 mb-30 wow fadeInUp" data-wow-delay=".5s">
+                            <div class="sasup-blog-box-img-4">
+                                <a href="blog-details.html"><img src="/assets/img/blog/blog-4-2.webp" alt="image not found"></a>
+                            </div>
+                            <div class="sasup-blog-box-content-4 pt-25">
+                                <a href="blog-details.html" class="sasup-blog-box-category-4 mb-10">Development</a>
+                                <h4 class="sasup-blog-box-title-4">
+                                    <nuxt-link to="/blog/1">Is your company facing a social media crisis?</nuxt-link>  </h4>
+                            </div>
+                            <div class="sasup-blog-box-meta-4">
+                                <span>April 12, 2022</span>
+                                <span>4 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                        <div class="sasup-blog-box-4 mb-30 wow fadeInUp" data-wow-delay=".7s">
+                            <div class="sasup-blog-box-img-4">
+                                <a href="blog-details.html"><img src="/assets/img/blog/blog-4-3.webp" alt="image not found"></a>
+                            </div>
+                            <div class="sasup-blog-box-content-4 pt-25">
+                                <a href="blog-details.html" class="sasup-blog-box-category-4 mb-10">Design Tools</a>
+                                <h4 class="sasup-blog-box-title-4">
+                                    <nuxt-link to="/blog/1">A guide to market your Saas product</nuxt-link>
+                                
+                                </h4>
+                            </div>
+                            <div class="sasup-blog-box-meta-4">
+                                <span>October 28, 2022</span>
+                                <span>4 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                        <div class="sasup-blog-box-4 mb-30 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="sasup-blog-box-img-4">
+                                <a href="blog-details.html"><img src="/assets/img/blog/blog-5.webp" alt="image not found"></a>
+                            </div>
+                            <div class="sasup-blog-box-content-4 pt-25">
+                                <a href="blog-details.html" class="sasup-blog-box-category-4 mb-10">Design Tools</a>
+                                <h4 class="sasup-blog-box-title-4"> 
+                                    <nuxt-link to="/blog/1">A guide to market your Saas product</nuxt-link>
+                                
+                                </h4>
+                            </div>
+                            <div class="sasup-blog-box-meta-4">
+                                <span>October 28, 2022</span>
+                                <span>4 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                        <div class="sasup-blog-box-4 mb-30 wow fadeInUp" data-wow-delay=".5s">
+                            <div class="sasup-blog-box-img-4">
+                                <a href="blog-details.html"><img src="/assets/img/blog/blog-4.webp" alt="image not found"></a>
+                            </div>
+                            <div class="sasup-blog-box-content-4 pt-25">
+                                <a href="blog-details.html" class="sasup-blog-box-category-4 mb-10">Development</a>
+                                <h4 class="sasup-blog-box-title-4">
+                                    <nuxt-link to="/blog/1">A guide to market your Saas product</nuxt-link>
+                                    
+                                </h4>
+                            </div>
+                            <div class="sasup-blog-box-meta-4">
+                                <span>April 12, 2022</span>
+                                <span>4 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                        <div class="sasup-blog-box-4 mb-30 wow fadeInUp" data-wow-delay=".7s">
+                            <div class="sasup-blog-box-img-4">
+                                <a href="blog-details.html"><img src="/assets/img/blog/blog-1.webp" alt="image not found"></a>
+                            </div>
+                            <div class="sasup-blog-box-content-4 pt-25">
+                                <a href="blog-details.html" class="sasup-blog-box-category-4 mb-10">Design Tools</a>
+                                <h4 class="sasup-blog-box-title-4">
+                                    <nuxt-link to="/blog/1">A guide to market your Saas product</nuxt-link>
+                                    
+                                </h4>
+                            </div>
+                            <div class="sasup-blog-box-meta-4">
+                                <span>October 28, 2022</span>
+                                <span>4 min read</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div class="col-xxl-12">
+                        <div class="basic-pagination mt-40">
+                            <ul>
+                                <li><a href="#" class="page-numbers">1</a></li>
+                                <li><span class="page-numbers current">2</span></li>
+                                <li><a href="#" class="page-numbers">3</a></li>
+                                <li><a href="#" class="next page-numbers"><i
+                                            class="fal fa-arrow-right"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- blog area end -->
+
+    </main>
+</template> 
